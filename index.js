8798b47f314dee95a95fd9605e7c8b3cdc47d198
Jose Sustaita <josesustaita70@gmail.com>
2020-06-10 21:04:29 -0400

    Connected routers and added custom middleware to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code away!
+const express = require("express");
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+const server = express();
+server.use(express.json());
+server.use(logger);
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Hello world!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(`${new Date().toDateString()} | ${req.method} | ${req.url}`);
+  next();
+}
+
+module.exports = server;
